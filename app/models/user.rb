e14712d89f6d23214f9f2571d77de6cf7fceb5c5
fegzycole <fergusoniyara@gmail.com>
2020-02-07 11:50:08 +0100

    Make username column unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
-  mount_uploader :photo, AvatarUploader
-  mount_uploader :cover_image, AvatarUploader
+  before_save :downcase_username
+  mount_uploader :photo, ImageUploader
+  mount_uploader :cover_image, ImageUploader
+  validates :username, presence: true, length: { maximum: 15 },
+                       uniqueness: { case_sensitive: false }
+  valdates :fullname, presence: true
+
+  private
+
+  def downcase_username
+    self.username = username.downcase
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,16 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_207_073_646) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2020_02_07_104854) do
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'fullname'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'photo'
-    t.string 'cover_image'
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "fullname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
+    t.string "cover_image"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
+
 end
