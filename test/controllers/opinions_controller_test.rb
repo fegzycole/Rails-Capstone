462ddff67573fe089f319c9f2394c0f4c230a3bd
fegzycole <fergusoniyara@gmail.com>
2020-02-12 08:04:46 +0100

    Add rspec resources

--- test/controllers/opinions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OpinionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get opinions_index_url
-    assert_response :success
-  end
-
-end
